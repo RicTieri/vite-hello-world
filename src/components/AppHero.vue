4811945e3ac48bde851f41129c4b0eae674a8ef5
RicTieri <tieri.riccardo@gmail.com>
2023-12-13 15:56:34 +0100

    added apptitle and apphero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="jumbotron">{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'jumbotron'
+    }
+  }
+}
+</script>
+
+<style>
+.jumbotron {
+  color: red;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="text-color">{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'La mia prima app con Vite!'
+    }
+  }
+}
+</script>
+
+<style>
+.text-color {
+  color: rgba(251, 251, 251, 0.557);
+}
+</style>
